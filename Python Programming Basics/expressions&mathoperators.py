ff91a0fb69d6cc47f03579b45029df84e2e37ea8
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2023-09-15 12:28:54 -0500

    small notes

---
+++ Python Programming Basics/expressions&mathoperators.py
@@ -0,0 +1,7 @@
+# Python has a vast array of libraries/functions and constructors but we will just be learning enough just o write useful programs.
+
+# Here are some math opperators for you to understand
+#  - (**) Exponent Operator ex: 2 ** 3 will evaluate to 8
+#  - (%) Modulus/remainder ex: 22 % 8 will evaluate to 6 because the remainder of 22 divided by 8 is 6
+#  - (//) interger division  ex: 22 // 8 will evaluate ot 2 because it will 
+2 + 2
