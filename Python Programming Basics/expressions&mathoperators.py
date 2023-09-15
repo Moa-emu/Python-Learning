385581b4f92f07332fe4c1ec7bfcb7f99cf0b4ad
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2023-09-15 12:29:04 -0500

    more changes

--- Python Programming Basics/expressions&mathoperators.py
+++ Python Programming Basics/expressions&mathoperators.py
@@ -3,5 +3,6 @@
 # Here are some math opperators for you to understand
 #  - (**) Exponent Operator ex: 2 ** 3 will evaluate to 8
 #  - (%) Modulus/remainder ex: 22 % 8 will evaluate to 6 because the remainder of 22 divided by 8 is 6
-#  - (//) interger division  ex: 22 // 8 will evaluate ot 2 because it will 
-2 + 2
+#  - (//) interger division  ex: 22 // 8 will evaluate ot 2 because it will give the highest 
+print(2 * 2)
+
