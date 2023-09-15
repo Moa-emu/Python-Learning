15f91e633dd2d6702347e9172f3ff9da9d61999c
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2023-09-15 13:04:45 -0500

    finishing the basic Math section

--- Python Programming Basics/expressions&mathoperators.py
+++ Python Programming Basics/expressions&mathoperators.py
@@ -1,8 +1,37 @@
 # Python has a vast array of libraries/functions and constructors but we will just be learning enough just o write useful programs.
+# Also the book that we are getting all of this information from is Automate the boring stuff with Python
+
 
 # Here are some math opperators for you to understand
 #  - (**) Exponent Operator ex: 2 ** 3 will evaluate to 8
 #  - (%) Modulus/remainder ex: 22 % 8 will evaluate to 6 because the remainder of 22 divided by 8 is 6
-#  - (//) interger division  ex: 22 // 8 will evaluate ot 2 because it will give the highest 
-print(2 * 2)
+#  - (//) interger division  ex: 22 // 8 will evaluate to 2 because it will give the highest floored qoutient
+#  - (/) Normal division  ex: 22 / 8 will evaluate to 2.75 because it will give the actual calculated divisio including decimal points
+#  - (*) Multiplication  ex: 3 * 5  This will evaluate to 15 just normal multiplication
+#  - (-) Subtraction ex: 5 - 2 This will evaluate to 3 because it is just normal subtraction
+#  - (+) Addition ex: 2 + 2 Thsi will evaluater to 4 because we are just doing basic additon
+
+# print will just print the output to the console/ terminal
+# also we make comments in pyton using # if you could not tell comments are useful for writing notes and explaining how code works
+
+#Below we will print the Exponent operator of 2^3
+print(2 ** 3)
+
+# Below we are using the modulus operator
+print(48 % 8)
+
+# below we are using the interger floored division operator
+print(48 // 8)
+
+#Below we are using the regular division operator which will include the decimal numbers
+print(48 / 3)
+
+#Below we are usign the Multiplication operator
+print(3 * 3)
+
+#Below we are using the addition operator
+print(3 + 3)
+
+#Below we are uising the subtraction operator
+print(3 - 3)
 
