f231990ea9ada8b53faec0d18807ee316cea27b2
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2023-09-16 11:45:49 -0500

    data types

---
+++ Python Programming Basics/intergers,floating-point,and-strings.py
@@ -0,0 +1,28 @@
+# Introducing data types: -this is a category for values, and every value belongs to exactly one data type.print
+
+#Below you will see the three common data types
+# - Intergers(int): These are whole numbers such as 2, -2, 3, 5, 600 and so on
+# - Floating-Point numbers(floats): These are numbers with decimal points such as 2.5, -2.5, 0.0, 40.0 and so on
+# - Strings(strs): These are text values that are surrounde by single qoutes ex: 'hello', '11 cats', 'aa', ' ' and so on you can notice that you can put even numbers and empty spaces(blank strings) as strings as long as you encase them in ''(qoutes)
+    # Note that if you ever see an error messsage that says SyntaxError it is probobly that you forgot to finish enasing your string in qoutes
+
+
+
+# String Concatenation and Replication
+    # - Although the (+)plus operator is used in mathmatical operations you can also use it to join two string together this iss called string Concatenation
+# example below:
+print('bob' + 'sarah')
+# that will output bobsarah
+# Quick Note that you can only connect strings with strings you cannot concatonate strings with any other data type or you will get errors unless you convert another data type into a string but that will be covered later
+
+
+
+#String Replication
+    # - You can use the *(Multiplication Operator) to replicate your string how ever many times that you want 
+#ex: 
+print('I love you sahra' * 5)
+# that will output: I love you sahraI love you sahraI love you sahraI love you sahraI love you sahra
+# Remember that with string Replication you can only do it with a string and an interger value anything else and you will get an error
+# Also Quick note that string concatonation is used more often then string replication but it is still a cool strick to remember
+
+
