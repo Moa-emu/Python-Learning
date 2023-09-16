b036eafe2d027a0a3c4dc8419b45e7f8c3e43017
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2023-09-16 12:00:14 -0500

    var

---
+++ Python Programming Basics/Variables.py
@@ -0,0 +1,14 @@
+#Variables and how to store values in them
+#   - A variable is just a way that you can store a value that you can use later
+#   - To assign a value to a variable all you have to do is use the asssignment operator 
+#   - example of using the assignment operator: money = 42
+#   -You can see in our example that we have a variable we named money and we assigned it to the value of 42
+#   - Remember that a variable is initialized/created after you assign it after that you can do whatever you want with it look below
+greeting = 'hello'
+print(greeting)
+
+
+money = 12
+hours = 12
+profit = money * hours
+print(profit)
