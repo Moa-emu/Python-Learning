fb293790b4773637a877966eb8d02a2eedc78f64
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2024-02-10 11:16:25 -0600

    new

---
+++ Function ch3/FunctionProjects/CrackingCode/Encrypting with the Transposition Cipher/EncryptingWithColumnarTranspositionCipher.py
@@ -0,0 +1,23 @@
+#read up on the transposition cipher and the code below will do what we need
+
+#below we are importing pyperclip so that we can copy the ciphertext there
+
+#below will be our def main() function
+def main():
+    #below the user will put the message they want to encrypt
+    userMessage = str(input('please put the message that you want to encrypt: '))
+    #below will be the key that the user wants to use
+    userKey = int(input('Type the key value you would like to use:'))
+
+
+
+#below will be a function that actualy encrypts the text
+    #below the function takes the key and messge arguments
+    def encryptMessage(key, message):
+        #below will contain a list data type we will learn more about this later. list take individual items/elements that can be whatever data type value as long as it does not conflic with your code
+        #each string in cipher text represents a column, since nthe number of columns is equal to the key the user uses
+        cipherText = [''] * key
+
+        #below will loop through each column in ciphertext
+        for column in range(key):
+            currentIndex = column
