496ee66e76e9211dc4f91680c0ab9c32e05f82dc
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2024-02-11 15:38:28 -0600

    new

---
+++ Flow Control ch2/OperatiorTruthTable.py
@@ -0,0 +1,41 @@
+# In Python, boolean operators are used to combine conditional statements. These operators include and, or, and not. Each operator returns a boolean value (True or False) based on the truthiness of the operands.
+
+# Here are the truth tables for the main boolean operators in Python:
+
+# Truth Table for and Operator
+# Expression	Result
+#__________________________________
+# True and True	True
+# True and False	False
+# False and True	False
+# False and False	False
+
+
+# Truth Table for or Operator
+# Expression	Result
+#__________________________
+# True or True	True
+# True or False	True
+# False or True	True
+# False or False	False
+
+
+# Truth Table for not Operator
+# Expression	Result
+#__________________________________
+# not True	False
+# not False	True
+
+
+# Additionally, Python includes comparison operators such as ==, !=, <, >, <=, and >=. These operators compare two values and return a boolean result. Here are some examples using comparison operators:
+
+x =  5
+y =  8
+
+print("x == y:", x == y)  # Output: False
+print("x != y:", x != y)  # Output: True
+print("x < y:", x < y)    # Output: True
+print("x > y:", x > y)    # Output: False
+print("x <= y:", x <= y)  # Output: True
+print("x >= y:", x >= y)  # Output: False
+# In the comparison operators, == checks for equality, != checks for inequality, < checks if the left operand is less than the right, > checks if the left operand is greater than the right, <= checks if the left operand is less than or equal to the right, and >= checks if the left operand is greater than or equal to the right
