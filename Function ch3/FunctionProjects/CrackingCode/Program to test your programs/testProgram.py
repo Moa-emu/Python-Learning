2d18ac105e2beec1061e6ce7c2803dcb9f24e961
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2024-02-12 09:37:25 -0600

    new

--- Function ch3/FunctionProjects/CrackingCode/Program to test your programs/testProgram.py
+++ Function ch3/FunctionProjects/CrackingCode/Program to test your programs/testProgram.py
@@ -6,4 +6,27 @@
 import random, sys, DecryptTranspositionCopy, EncryptingWithColumnarTranspositionCipherCopy
 
 def main():
-    
+    #below we will be seeding a random number then using the random.randint.
+    #the difference is that when you seed you get a pseudo random number it is not really random and you can predict the pattern
+    #without seeding you will get an actual random number
+    random.seed(42)# seeding random numbers is good for testing your programs because you want predictable numbers so the same pseudorandom messages and keys are chosen
+
+
+    #Below will be a for loop that will fun 20 test 
+    for i in range(20):
+
+        #below will generate random messages to test
+        #we will be multiplying the message string by a random number, think like this 'ABC' * 3 = 'ABCABCABC'
+        #then after multiplying later down we will shuffle that string to have more random text
+        message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' * random.randint(4, 40)
+
+
+
+
+
+
+
+
+#if test.py is run(instead of imported as a module) call the main() function
+if __name__ == '__main__':
+    main()
