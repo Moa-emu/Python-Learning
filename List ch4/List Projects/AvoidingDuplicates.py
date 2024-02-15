1bfe7a7fd9d6903f75579ce657f3ecfee168df49
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2024-02-15 12:21:40 -0600

    new

--- List ch4/List Projects/AvoidingDuplicates.py
+++ List ch4/List Projects/AvoidingDuplicates.py
@@ -1,5 +1,21 @@
 # This is Exercise 107
 
+# In this exercise, you will create a program that reads words from the user until the
+# user enters a blank line. After the user enters a blank line your program should display each word entered by the user exactly once. The words should be displayed in
+# the same order that they were entered. 
+
+#For example, if the user enters:
+# first
+# second
+# first
+# third
+# second
+
+# then your program should display:
+# first
+# second
+# third
+
 def main():
     #below will be an empty list that will contain everything the user adds
     usersList = []
