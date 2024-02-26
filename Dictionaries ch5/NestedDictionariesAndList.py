7adb777dada0c06a397f106e8d54ec92b1412808
Moa-emu <85137704+Moa-emu@users.noreply.github.com>
2024-02-26 10:32:23 -0600

    now

---
+++ Dictionaries ch5/NestedDictionariesAndList.py
@@ -0,0 +1,5 @@
+# NESTED DICTIONARIES AND LIST
+
+#   - As you model more and more complicated things,you may find you need
+#dictionaires  and list that contain other dictionaries and lists.Below will
+#Be an example of how you can use this concept.
